fc5694ad5a12a63d9f787913c345f7b751ac2dcc
rian-joaquim <rian.joaquim@usemaisbrasil.com.br>
2026-08-10 20:57:31 -0300

    fix(auth): adicionar tipo de retorno explícito ao componente Welcome/Navigation

--- app/Livewire/Welcome/Navigation.php
+++ app/Livewire/Welcome/Navigation.php
@@ -2,11 +2,12 @@
 
 namespace App\Livewire\Welcome;
 
+use Illuminate\View\View;
 use Livewire\Component;
 
 class Navigation extends Component
 {
-    public function render()
+    public function render(): View
     {
         return view('livewire.welcome.navigation');
     }
