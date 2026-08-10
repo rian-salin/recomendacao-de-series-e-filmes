4dd2740ba1b660d4a6e4b6c3c1cdc69872b7b0e4
rian-joaquim <rian.joaquim@usemaisbrasil.com.br>
2026-08-10 20:46:04 -0300

    chore: remover Breeze, Volt e todos os arquivos de auth gerados por eles

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome');
 
 Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
+    ->middleware('auth')
     ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
 
 require __DIR__.'/auth.php';
